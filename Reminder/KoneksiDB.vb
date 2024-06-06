1a892da1d653e48263330f7f75e04f386e75ff60
Moch Rizaldi Ananda Kurnia <mochrizaldianandakurnia21@gmail.com>
2024-06-06 08:54:06 +0700

    Create module koneksi database and change Form1 to Login

---
+++ Reminder/KoneksiDB.vb
@@ -0,0 +1,17 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module KoneksiDB
+    Public Conn As MySqlConnection
+    Public DR As MySqlDataReader
+    Public DA As MySqlDataAdapter
+    Public CMD As MySqlCommand
+    Public DS As DataSet
+
+    Public Sub BukaDb()
+        Dim SQLConn As String = "server=localhost;uid=root;Database=reminder;SSLMode=none"
+        Conn = New MySqlConnection(SQLConn)
+        If Conn.State = ConnectionState.Closed Then
+            Conn.Open()
+        End If
+    End Sub
+End Module
--- Reminder/Login.Designer.vb
+++ Reminder/Login.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
-Partial Class Form1
+Partial Class Login
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,16 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        SuspendLayout()
+        ' 
+        ' Login
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Name = "Login"
+        Text = "Login"
+        ResumeLayout(False)
     End Sub
 
 End Class
